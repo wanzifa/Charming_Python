0242800b263de6102d613338c937b4ce4ed7d21e
wanzifa <wanzifun@gmail.com>
2016-03-18 23:48:23 +0800

    complete werkzeug

--- werkzeug模块小应用/shortly/shortly.py
+++ werkzeug模块小应用/shortly/shortly.py
@@ -18,10 +18,19 @@ from jinja2 import Environment, FileSystemLoader
 class Shortly(object):
 
     def __init__(self, config):
-        self.redis = redis.Redis(config['redis_host'], config['redis_port'])
+        self.redis = redis.Redis(config['redis_host'], config['redis_port'])  
         template_path = os.path.join(os.path.dirname(__file__), 'templates')
+        #autoescape=True的意思，就是xml/html不转义
         self.jinja_env = Environment(loader=FileSystemLoader(template_path),
-                                    autoescape=True)
+                                     autoescape=True)
+        #Map创建的这个实例，就像它名字一样，是一个url与函数端点一一对应的“地图”
+        #参数是一个列表，起一个关系集中的作用
+        self.url_map = Map([
+            #Rule类，创建具体的函数端点的对应关系
+            Rule('/', endpoint='new_url'),
+            Rule('/<short_id>', endpoint='follow_short_link'),
+            Rule('/<short_id>+', endpoint='short_link_details')
+        ])
 
     def render_template(self, template_name, **context):
         t = self.jinja_env.get_template(template_name)
@@ -30,45 +39,50 @@ class Shortly(object):
     def dispatch_request(self, request):
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
+            #返回函数的端点名，以及函数所需的参数字典
             endpoint, values = adapter.match()
             return getattr(self, 'on_'+endpoint)(request, **values)
         except HTTPException, e:
             return e
 
     def wsgi_app(self, environ, start_response):
+        #Request接收environ中的内容，把它们封装
+        #它提供的可访问对象都是只读的
         request = Request(environ)
         response = self.dispatch_request(request)
+        #不同于请求对象，响应对象是可修改的
         return response(environ, start_response)
     
     def __call__(self, environ, start_response):
         return self.wsgi_app(environ, start_response)
     
+    @staticmethod
+    def is_valid_url(url):
+        parts = urlparse.urlparse(url)
+        return parts.scheme in ('http', 'https')
+
+    def insert_url(self, url):
+        short_id = self.redis.get('reverse-url:' + url)
+        if short_id is not None:
+            return short_id
+        url_num = self.redis.incr('last-url-id')
+        short_id = base36_encode(url_num)
+        self.redis.set('url-target:' + short_id, url)
+        self.redis.set('reverse-url:' + url, short_id)
+        return short_id
+ 
     def on_new_url(self, request):
         error = None
         url = ''
         if request.method == 'POST':
             url = request.form['url']
-            if not is_valid_url(url):
+            if not self.is_valid_url(url):
                 error = 'Please enter a valid URL'
             else:
                 short_id = self.insert_url(url)
                 return redirect('%s+'%short_id)
         return self.render_template('new_url.html', error=error)
    
-    def is_valid_url(url):
-        parts = urlparse.urlparse(url)
-        return parts.scheme in ('http', 'https')
-
-    def insert_url(self.url):
-        short_id = self.redis.get('reverse-url:' + url)
-        if short_id is not None:
-            return short_id
-        url_num = self.redis.incr('last-url-id')
-        short_id = base36_encode(url_num)
-        self.redis.get('url-target:' + short_id, url)
-        self.redis.set('reverse-url:' + url, short_id)
-        return short_id
-    
     def on_follow_short_link(self, request, short_id):
         link_target = self.redis.get('url-target:' + short_id)
         if link_target is None:
@@ -77,10 +91,10 @@ class Shortly(object):
         return redirect(link_target)
 
     def on_short_link_details(self, request, short_id):
-        link_target = self.redis.get('url_target:' + short_id)
+        link_target = self.redis.get('url-target:' + short_id)
         if link_target is None:
             raise NotFound()
-        click_count = int(self.redis.get('click-count:' + short_id or 0))
+        click_count = int(self.redis.get('click-count:' + short_id) or 0)
         return self.render_template('short_link_details.html',
             link_target=link_target,
             short_id=short_id,
@@ -98,7 +112,7 @@ def base36_encode(number):
     return ''.join(reversed(base36))
 
 
-def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+def create_app(redis_host='127.0.0.1', redis_port=6379, with_static=True):
     app = Shortly({
         'redis_host': redis_host,
         'redis_port': redis_port
